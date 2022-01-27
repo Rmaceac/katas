a2f223021b7d7c1bc646788c478dfb65c61a8492
Ryan MacEachern <ryan.maceachern@gmail.com>
2022-01-26 21:41:45 -0800

    Completed the first kata assignment

--- k1-sum-the-largest.js
+++ k1-sum-the-largest.js
@@ -1,3 +1,18 @@
 const sumLargestNumbers = function(data) {
-  
-}
+  let firstNum = 0;
+  let secondNum = 0;
+  for (let num of data) {
+    for (num of data) {
+      if (num > firstNum) {
+        firstNum = num;
+    } else if (num < firstNum && num > secondNum) {
+        secondNum = num;
+      } 
+    }
+  }
+  return firstNum + secondNum;
+};
+
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
